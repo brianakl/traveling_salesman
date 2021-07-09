6e8d66d1ca4fc1da39871015f00c55dba186d47a
Brian Naklycky <69728590+brianakl@users.noreply.github.com>
2021-07-09 08:03:57 -0400

    ortools tsp solution

--- or_tools_test.cc
+++ or_tools_test.cc
@@ -1,6 +1,13 @@
 #include <iostream>
+#include <fstream>
 #include "ortools/linear_solver/linear_solver.h"
 #include "ortools/sat/cp_model.h"
+#include <vector>
+#include "ortools/constraint_solver/routing.h"
+#include "ortools/constraint_solver/routing_enums.pb.h"
+#include "ortools/constraint_solver/routing_index_manager.h"
+#include "ortools/constraint_solver/routing_parameters.h"
+
 
 
 using namespace std;
@@ -126,9 +133,6 @@ namespace operations_research{
             {3, 6, 5, int(INFINITY)}
         };
         int n = 4;
-        vector<vector<MPVariable*>* >* vec;
-        vector<vector<MPConstraint*>* >* constraints;
-        vector<MPVariable*> *v;
 
         unique_ptr<MPSolver> solver(MPSolver::CreateSolver("GLOP"));
         const double infinity = solver->infinity();
@@ -139,7 +143,7 @@ namespace operations_research{
         
         //solver->MakeNumVarArray(n, 0.0, 1.0, "row0", v);
 
-
+        /*
         for(int i = 0; i < n; i++){
             vec->push_back(new vector<MPVariable*>());
             for (int j = 0; j < n; j++){
@@ -147,11 +151,22 @@ namespace operations_research{
             }
         }
 
+        */
+        MPVariable* const x_0 = solver->MakeIntVar(0.0, 3.0, "x_0");
+        MPVariable* const x_1 = solver->MakeIntVar(0.0, 3.0, "x_1");
+        MPVariable* const x_2 = solver->MakeIntVar(0.0, 3.0, "x_2");
+        MPVariable* const x_3 = solver->MakeIntVar(0.0, 3.0, "x_3");
+
+
+        
+
         LOG(INFO) << "Number of variables:\t" << solver->NumVariables();
-        //MPConstraint* const c0 = solver->MakeRowConstraint();
+        MPConstraint* const c0 = solver->MakeRowConstraint();
+        
         //c0->SetCoefficient();
 
         //setting constraints
+        /*
         for(int i = 0; i < n; i++){
             constraints->push_back(new vector<MPConstraint*>());
             for (int j = 0; j < n; j++){
@@ -160,13 +175,14 @@ namespace operations_research{
             }
         }
 
-        //solver->MakeNumVarArray(n, 0.0, 1.0, "row0", vec->at(0));
-        //solver->MakeNumVarArray(n, 0.0, 1.0, "row1", vec->at(1));            
-        //solver->MakeNumVarArray(n, 0.0, 1.0, "row2", vec->at(2));
-        //solver->MakeNumVarArray(n, 0.0, 1.0, "row3", vec->at(3));
 
-       //MakeNumVar(double lower bound, double upper bound, var name)
-       //MPVariable* const x = solver->MakeNumVar(0.0,1.0, "x");
+
+        solver->MakeNumVarArray(n, 0.0, 1.0, "row1", vec->at(1));            
+        solver->MakeNumVarArray(n, 0.0, 1.0, "row2", vec->at(2));
+        solver->MakeNumVarArray(n, 0.0, 1.0, "row3", vec->at(3));
+
+        MakeNumVar(double lower bound, double upper bound, var name)
+        MPVariable* const x = solver->MakeNumVar(0.0,1.0, "x");
        
 
         // Σ ∀
@@ -175,75 +191,68 @@ namespace operations_research{
         //minimize Σ(n,i=1)[Σ(n,j=1)[c_ij * x_ij] ]
         
         MPObjective* const objective = solver->MutableObjective();
-        objective->SetMinimization();        
+        objective->SetMinimization();
+        objective->SetCoefficient(x_0,dist_m[0][x_3->solution_value()]);
+        objective->SetCoefficient(x_1,dist_m[1][x_0->solution_value()]);
+        objective->SetCoefficient(x_2,dist_m[2][x_1->solution_value()]);
+        objective->SetCoefficient(x_3,dist_m[3][x_2->solution_value()]);
 
+        LOG(INFO) << objective->Value();
+        */
+
+
+
+
+        /*
         for (int i = 1; i < n; i++){
             for (int j = 1; j < i-1; j++){
                 objective->SetCoefficient(vec->at(i)->at(j), dist_m[i][j]);
             }
         }
+        */
         //constraints
         // ∀(i) 1 <= i <= n     
         // ∀(ij) 1 <= j < i <= n
         // Σ(j<i)[x_ij] + Σ(j>i)[x_ji] = 2
-
-
-        //loop through every possible set of 
-
-
-        //make constraints
-
-        //MPConstraint* const c0 = solver->MakeRowConstraint();
 
     }
 
     namespace sat{
         void tsp_cp(){
             CpModelBuilder cp_model;
-            double dist_m [4][4] = {
-                {0, 1, 1, 4},
-                {8, 0, 2, 6},
-                {9, 7, 0, 2},
-                {3, 6, 5, 0}
+
+            //test graph
+            //shortest path = 0 -> 1 -> 2 -> 3 -> 0
+            int dist_m [4][4] = {
+                {int(INFINITY), 1, 1, 4},
+                {8, int(INFINITY), 2, 6},
+                {9, 7, int(INFINITY), 2},
+                {3, 6, 5, int(INFINITY)}
             };
             int n = 4;
 
             const Domain domain(0.0,1.0);
 
-            //variables
-            vector<vector<IntVar> > v;
-
-            for (int i = 0; i < n; i++){
-                v.push_back(vector<IntVar>());
-                for (int j = 0; j < n; j++){
-                    if (i == j) continue;
-                    v[i].push_back(cp_model.NewIntVar(domain).WithName("x_" + to_string(i)+ to_string(j)));
-                }
-            }
-
-            for (int i = 0; i < n; i++){
-                for (int j = 0; j <n; j++)
-                    v[i][j].AddConstant(dist_m[i][j]);
-            }
-
-            const IntVar x_01 = cp_model.NewIntVar(domain).WithName("x01");
 
 
 
+            
+            const IntVar x_0 = cp_model.NewIntVar(domain).WithName("x0");
+            const IntVar x_1 = cp_model.NewIntVar(domain).WithName("x1");
+            const IntVar x_2 = cp_model.NewIntVar(domain).WithName("x2");
+            const IntVar x_3 = cp_model.NewIntVar(domain).WithName("x3");
 
-            /*
-            const IntVar x_02 = cp_model.NewIntVar(domain).WithName("x02");
-            const IntVar x_03 = cp_model.NewIntVar(domain).WithName("x03");
-            const IntVar x_10 = cp_model.NewIntVar(domain).WithName("x10");
-            const IntVar x_12 = cp_model.NewIntVar(domain).WithName("x12");
-            const IntVar x_13 = cp_model.NewIntVar(domain).WithName("x13");
-            const IntVar x_20 = cp_model.NewIntVar(domain).WithName("x20");
-            const IntVar x_21 = cp_model.NewIntVar(domain).WithName("x21");
-            const IntVar x_23 = cp_model.NewIntVar(domain).WithName("x23");
-            const IntVar x_30 = cp_model.NewIntVar(domain).WithName("x30");
-            const IntVar x_31 = cp_model.NewIntVar(domain).WithName("x31");
-            const IntVar x_32 = cp_model.NewIntVar(domain).WithName("x32");
-            */
+            IntVar vars[4];
+            vars[0] = cp_model.NewIntVar(domain).WithName("x0");
+            vars[1] = cp_model.NewIntVar(domain).WithName("x1");
+            vars[2] = cp_model.NewIntVar(domain).WithName("x2");
+            vars[3] = cp_model.NewIntVar(domain).WithName("x3");
+
+            cp_model.AddAllDifferent(vars);
+
+
+            
+            
             //constraints
             
             
@@ -252,14 +261,156 @@ namespace operations_research{
             
         }
     }
+
+    struct DataModel{
+        /*
+        const vector<vector<int> > dist_matrix{
+            {int(INFINITY), 1, 1, 4},
+            {8, int(INFINITY), 2, 6},
+            {9, 7, int(INFINITY), 2},
+            {3, 6, 5, int(INFINITY)}
+        };
+        const vector<vector<int> > dist_matrix{
+            {0,	66,	9, 85, 7, 47, 18, 45, 45, 78, 23},
+            {5,	0,	81, 98,	45, 43, 45, 36, 82, 89, 22},
+            {71, 92, 0, 43, 77, 93, 74, 71, 23, 17, 90},
+            {40, 49, 18, 0, 32, 88, 56, 52, 93, 1, 42},
+            {40, 37, 67, 58, 0, 47, 89, 21, 20, 14, 53},
+            {4,	37,	9, 11, 98, 0, 9, 42, 1, 85, 12},
+            {3,	66, 8, 43, 50, 98, 0, 29, 19, 66, 61},
+            {48, 48, 3,	81,	95,	28, 60, 0, 47, 30, 39},
+            {100, 17, 75, 49, 79, 79, 42, 0, 29, 49},
+            {44, 13,	59,	15,	99,	75,	53,	47, 0, 82},
+            {10,2,	71,	99,	80,	35,	65,	76, 79, 0}
+        };
+       
+
+        vector<vector<int> > dist_matrix {
+            {0, 29, 82, 46, 68, 52, 72, 42, 51, 55, 29, 74, 23, 72, 46 },
+            {29,  0, 55, 46, 42, 43, 43, 23, 23, 31, 41, 51, 11, 52, 21 },
+            {82, 55,  0, 68, 46, 55, 23, 43, 41, 29, 79, 21, 64, 31, 51 },
+            {46, 46, 68,  0, 82, 15, 72, 31, 62, 42, 21, 51, 51, 43, 64 },
+            {68, 42, 46, 82,  0, 74, 23, 52, 21, 46, 82, 58, 46, 65, 23 },
+            {52, 43, 55, 15, 74,  0, 61, 23, 55, 31, 33, 37, 51, 29, 59 },
+            {72, 43, 23, 72, 23, 61,  0, 42, 23, 31, 77, 37, 51, 46, 33 },
+            {42, 23, 43, 31, 52, 23, 42,  0, 33, 15, 37, 33, 33, 31, 37 },
+            {51, 23, 41, 62, 21, 55, 23, 33,  0, 29, 62, 46, 29, 51, 11 },
+            {55, 31, 29, 42, 46, 31, 31, 15, 29,  0, 51, 21, 41, 23, 37 },
+            {29, 41, 79, 21, 82, 33, 77, 37, 62, 51,  0, 65, 42, 59, 61 },
+            {74, 51, 21, 51, 58, 37, 37, 33, 46, 21, 65,  0, 61, 11, 55 },
+            {23, 11, 64, 51, 46, 51, 51, 33, 29, 41, 42, 61,  0, 62, 23 },
+            {72, 52, 31, 43, 65, 29, 46, 31, 51, 23, 59, 11, 62,  0, 59 },
+            {46, 21, 51, 64, 23, 59, 33, 37, 11, 37, 61, 55, 23, 59,  0},
+            };
+        */
+
+        const int num_vehicles = 1;
+        const RoutingIndexManager::NodeIndex depot{0};
+        
+    };
+
+    void tsp(){
+        //DataModel data;
+
+        int n;
+
+        vector<vector<int> > dist_matrix;
+        const int num_vehicles = 1;
+        const RoutingIndexManager::NodeIndex depot{0};
+
+        string fileName = "1000_tsp.txt";    
+        ifstream ifile;
+        ifile.open(fileName);
+        if (!ifile){
+            cout << "File name: " << fileName << " does not exist" << endl;
+            exit(1);
+        }
+
+        ifile >> n;
+
+        
+        //dist matrix input
+        /*
+
+        for(int i = 0; i < n; i++){
+            dist_matrix.push_back(vector<int>());
+            for(int j = 0; j < n; j++){
+                if (i == j){
+                    dist_matrix[i].push_back(0);
+                    continue;
+                }
+                int k;
+                ifile >> k;
+                dist_matrix[i].push_back(k);
+            }
+        }
+        */
+
+       //coordinates
+        double f = 0, s = 0;
+        vector<pair<double, double> > coords;
+        for (int i = 0; i < n; i++){
+        ifile >> f >> s;
+        coords.push_back(make_pair(f,s));
+        }
+        //add edges
+        double x1,x2,y1,y2;
+        x1 = x2 = y1 = y2 = 0.0;
+        //calculates the edge weights for a graph that is euclidean 
+        for (int i = 1; i < n; i++){
+            for (int j = 0; j < n; j++){
+                if (i == j) {
+                    dist_matrix[i].push_back(0);
+                    continue;
+                }
+                x1 = coords[i].first;
+                y1 = coords[i].second;
+                x2 = coords[j].first;
+                y2 = coords[j].second;
+                dist_matrix[i].push_back((pow((x2-x1),2) + pow((y2-y1),2)));
+            }
+        }
+
+        RoutingIndexManager manager(n, num_vehicles, depot);
+
+        RoutingModel routing(manager);
+
+        const int transit_callback_index = routing.RegisterTransitCallback([&dist_matrix, &manager](int64_t from_index, int64_t to_index) -> int64_t {
+            auto from_node = manager.IndexToNode(from_index).value();
+            auto to_node = manager.IndexToNode(to_index).value();
+            return dist_matrix[from_node][to_node];
+        });
+
+        routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index);
+
+        RoutingSearchParameters searchParams = DefaultRoutingSearchParameters();
+        searchParams.set_first_solution_strategy(FirstSolutionStrategy::PATH_CHEAPEST_ARC);
+
+        const Assignment* solution = routing.SolveWithParameters(searchParams);
+
+        LOG(INFO) << "Objective: " << solution->ObjectiveValue() << " miles";
+        int64_t index = routing.Start(0);
+        LOG(INFO) << "Route: ";
+        int64_t distance{0};
+        stringstream route;
+        while (routing.IsEnd(index) == false){
+            route << manager.IndexToNode(index).value() << " -> ";
+            int64_t prev = index;
+            index = solution->Value(routing.NextVar(index));
+            distance += routing.GetArcCostForVehicle(prev, index, int64_t{0});
+        }
+        LOG(INFO) << route.str() << manager.IndexToNode(index).value();
+        LOG(INFO) << "Route distance: " << distance << " miles";
+        LOG(INFO) << "Problem solved in " << routing.solver()->wall_time() << "ms";
+    }
 }
 
 
 int main() {
 
-    operations_research::SimpleMipProgram();
+    //operations_research::SimpleMipProgram();
 
-    operations_research::sat::tsp_cp();
+    operations_research::tsp();
 
-    return EXIT_SUCCESS;
+    return 0;
 }
